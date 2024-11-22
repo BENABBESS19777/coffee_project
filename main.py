c1783dfa31d961449053247b0cffdf5d18b55589
BEN ABBESS MED ALI <benabbessmedali@gmail.com>
2024-11-22 20:44:45 +0000

    final program

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from os import waitstatus_to_exitcode
+import os
 
 MENU = {
     "espresso": {
@@ -31,119 +31,72 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
-#VARIABLES
 
-Money = 0
-#stock
 
-water_stock = resources['water']
-milk_stock = resources['milk']
-coffee_stock = resources['coffee']
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-#espresso description
 
-water_espresso = MENU['espresso']["ingredients"]["water"]
-coffee_espresso = MENU['espresso']["ingredients"]["coffee"]
-cost_espresso = MENU['espresso']["cost"]
-#latte description
-
-water_latte = MENU['latte']["ingredients"]["water"]
-milk_latte = MENU['latte']["ingredients"]["milk"]
-coffee_latte = MENU['latte']["ingredients"]["coffee"]
-cost_latte = MENU['latte']["cost"]
-#cappuccino description
-
-water_cappuccino = MENU['cappuccino']["ingredients"]["water"]
-milk_cappuccino = MENU['cappuccino']["ingredients"]["milk"]
-coffee_cappuccino = MENU['cappuccino']["ingredients"]["coffee"]
-cost_cappuccino = MENU['cappuccino']["cost"]
-
-#ALL FUNCTIONS
-
-def add_coins_inserted ():
+def add_coins_inserted(quarters, dimes, nickles, pennies):
     """ Calculate the monetary value of the coins inserted """
-    return quarters_numbers * 0.25 + dimes_numbers * 0.1 + nickles_numbers * 0.05 + pennies_numbers * 0.01
-def change_coins_inserted():
+    return quarters * 0.25 + dimes * 0.1 + nickles * 0.05 + pennies * 0.01
+
+
+def change_coins_inserted(monetary_value_inserted, coffee_cost):
     """Return money in change"""
-    return monetary_value_inserted - 1.5
+    return round(monetary_value_inserted - coffee_cost, 2)
+
+
 def print_report():
     """Shows the current resources values"""
-    print(f"water : {water_stock} ml")
-    print(f"milk : {milk_stock} ml")
-    print(f"coffee : {coffee_stock} g")
-    print(f"Money : {Money} $")
-def coffee_choice(coffee):
-    pass
+    print(f"Water: {resources['water']} ml")
+    print(f"Milk: {resources['milk']} ml")
+    print(f"Coffee: {resources['coffee']} g")
+    print(f"Money: ${Money}")
 
 
+def check_resources(coffee):
+    """Check if there are enough resources to make the coffee"""
+    for ingredient in MENU[coffee]["ingredients"]:
+        if resources[ingredient] < MENU[coffee]["ingredients"][ingredient]:
+            print(f"Sorry, there is not enough {ingredient}.")
+            return False
+    return True
 
-# print start program
-user_choice = input("What would you like? (espresso/ latte/ cappuccino) :")
-print(user_choice)
-#Report
-if user_choice == 'report':
-    print_report()
-#Espresso
-elif user_choice == 'Espresso':
-    print("please insert coins.")
-    quarters_numbers = int(input("How many quarters? : "))
-    dimes_numbers = int(input("How many dimes? : "))
-    nickles_numbers = int( input("How many nickles? : "))
-    pennies_numbers = int(input("How many quarters? : "))
-    monetary_value_inserted = add_coins_inserted()
-    if monetary_value_inserted == MENU['espresso']["cost"] :
-        print("Here is your Espresso ,Enjoy !")
-        water_stock-=  MENU['espresso']["ingredients"]["water"]
-        milk_stock -= 0
-        coffee_stock -= MENU['espresso']["ingredients"]["coffee"]
-    elif monetary_value_inserted > MENU['espresso']["cost"] :
-        print(f"Here is {change_coins_inserted()} in change ")
-        print("Here is your Espresso ,Enjoy !")
-        water_stock -= MENU['espresso']["ingredients"]["water"]
-        milk_stock -= 0
-        coffee_stock -= MENU['espresso']["ingredients"]["coffee"]
+
+Money = 0
+make_coffee = True
+
+while make_coffee:
+    clear_terminal()
+    user_choice = input(
+        "What would you like? (espresso/ latte/ cappuccino) or type 'report' to see resources or 'off' to turn off: ").lower()
+
+    if user_choice == 'report':
+        print_report()
+    elif user_choice == 'off':
+        make_coffee = False
+    elif user_choice in MENU:
+        if check_resources(user_choice):
+            print("Please insert coins.")
+            quarters_numbers = int(input("How many quarters? : "))
+            dimes_numbers = int(input("How many dimes? : "))
+            nickles_numbers = int(input("How many nickles? : "))
+            pennies_numbers = int(input("How many pennies? : "))
+
+            monetary_value_inserted = add_coins_inserted(quarters_numbers, dimes_numbers, nickles_numbers,
+                                                         pennies_numbers)
+            coffee_cost = MENU[user_choice]["cost"]
+
+            if monetary_value_inserted >= coffee_cost:
+                change = change_coins_inserted(monetary_value_inserted, coffee_cost)
+                Money += coffee_cost
+                for ingredient in MENU[user_choice]["ingredients"]:
+                    resources[ingredient] -= MENU[user_choice]["ingredients"][ingredient]
+                print(f"Here is your {user_choice}, enjoy!")
+                if change > 0:
+                    print(f"Here is ${change} in change.")
+            else:
+                print("Sorry that's not enough money. Money refunded.")
     else:
-        print("Sorry that's not enough money, Money refunded")
-#Latte
-elif user_choice == 'latte':
-    print("please insert coins.")
-    quarters_numbers = int(input("How many quarters? : "))
-    dimes_numbers = int(input("How many dimes? : "))
-    nickles_numbers = int( input("How many nickles? : "))
-    pennies_numbers = int(input("How many quarters? : "))
-    monetary_value_inserted = add_coins_inserted()
-    if monetary_value_inserted == MENU['latte']["cost"] :
-        print("Here is your latte ,Enjoy !")
-        water_stock-=  MENU['latte']["ingredients"]["water"]
-        milk_stock -= 0
-        coffee_stock -= MENU['latte']["ingredients"]["coffee"]
-    elif monetary_value_inserted > MENU['latte']["cost"] :
-        print(f"Here is {change_coins_inserted()} in change ")
-        print("Here is your latte ,Enjoy !")
-        water_stock -= MENU['latte']["ingredients"]["water"]
-        milk_stock -= MENU['latte']["ingredients"]["milk"]
-        coffee_stock -= MENU['latte']["ingredients"]["coffee"]
-    else:
-        print("Sorry that's not enough money, Money refunded")
-
-#Cappucino
-elif user_choice == 'cappuccino':
-    print("please insert coins.")
-    quarters_numbers = int(input("How many quarters? : "))
-    dimes_numbers = int(input("How many dimes? : "))
-    nickles_numbers = int( input("How many nickles? : "))
-    pennies_numbers = int(input("How many quarters? : "))
-    monetary_value_inserted = add_coins_inserted()
-    if monetary_value_inserted == MENU['cappuccino']["cost"] :
-        print("Here is your cappuccino ,Enjoy !")
-        water_stock-=  MENU['cappuccino']["ingredients"]["water"]
-        milk_stock -= MENU['cappuccino']["ingredients"]["milk"]
-        coffee_stock -= MENU['cappuccino']["ingredients"]["coffee"]
-    elif monetary_value_inserted > MENU['cappuccino']["cost"] :
-        print(f"Here is {change_coins_inserted()} in change ")
-        print("Here is your latte ,Enjoy !")
-        water_stock -= MENU['cappuccino']["ingredients"]["water"]
-        milk_stock -= 0
-        coffee_stock -= MENU['cappuccino']["ingredients"]["coffee"]
-    else:
-        print("Sorry that's not enough money, Money refunded")
+        print("Invalid choice. Please try again.")
